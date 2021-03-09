4ac31486f4fa35e60f1aa2dcdc190bdca35b36ef
DarioMC <dariomc_95@yahoo.es>
2021-03-09 12:54:31 -0800

    first commit

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+
+
+namespace core_crud_js.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+
+        private readonly ILogger<PersonaController> _logger;
+
+        public PersonaController(ILogger<PersonaController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        public ActionResult Get()
+        {
+            using (core_crud_js.Models.CrudpocContext db = new core_crud_js.Models.CrudpocContext())
+            {
+                var lst = (from d in db.Personas
+                           select d).ToList();
+                return Ok(lst);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Models.Request.PersonaRequest model)
+        {
+            using (core_crud_js.Models.CrudpocContext db = new core_crud_js.Models.CrudpocContext())
+            {
+                core_crud_js.Models.Persona oPersona = new core_crud_js.Models.Persona();
+                oPersona.Nombre = model.Nombre;
+                oPersona.Edad = model.Edad;
+                db.Personas.Add(oPersona);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpPut]
+        public ActionResult Put([FromBody] Models.Request.PersonaEditRequest model)
+        {
+            using (core_crud_js.Models.CrudpocContext db = new core_crud_js.Models.CrudpocContext())
+            {
+                core_crud_js.Models.Persona oPersona = db.Personas.Find(model.Id);
+                oPersona.Nombre = model.Nombre;
+                oPersona.Edad = model.Edad;
+                db.Entry(oPersona).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromBody] Models.Request.PersonaEditRequest model)
+        {
+            using (core_crud_js.Models.CrudpocContext db = new core_crud_js.Models.CrudpocContext())
+            {
+                core_crud_js.Models.Persona oPersona = db.Personas.Find(model.Id);
+                db.Personas.Remove(oPersona);
+                db.SaveChanges();
+            }
+
+            return Ok();
+        }
+
+
+
+    }
+}
---
+++ Models/CrudpocContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace core_crud_js.Models
+{
+    public partial class CrudpocContext : DbContext
+    {
+        public CrudpocContext()
+        {
+        }
+
+        public CrudpocContext(DbContextOptions<CrudpocContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Personas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost;Database=Crudpoc;Trusted_connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.ToTable("persona");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Edad).HasColumnName("edad");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("nombre");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
